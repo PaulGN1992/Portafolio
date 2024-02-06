352a9893f07ecd5b91db112d07f04634ff172464
PAULGN1992 <47513033@continental.edu.pe>
2024-02-06 16:26:58 -0500

    segundo dia

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'rojoPg': '#f22222',
+				'naranjaPg': '#f4422a',
+				'blancoPg': '#f2e2ce',
+				'negroPg': '#161616',
+				'grisPg': '#2f2f2f',
+				'limaPg': '#C6F52A',
+
+			  },
+		},
 	},
 	plugins: [],
 }
